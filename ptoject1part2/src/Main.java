fe3c85b944df10a41cce5120f2f59d295f910e34
Mirna <70171117+mirnama@users.noreply.github.com>
2022-09-10 17:52:31 -0600

    Assignment fished

--- ptoject1part2/src/Main.java
+++ ptoject1part2/src/Main.java
@@ -3,10 +3,11 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
 
-        Reader reader = new Reader();
+        Reader reader = new Reader(); // instantiate the objects
         Analyzer analyzer = new Analyzer();
-        ArrayList<Integer> data = reader.readInput();
-        analyzer.analyze(data);
+        ArrayList<Integer> data = reader.readInput(); // read data from the user
+        analyzer.analyze(data); // perform all calculations
+
 
         System.out.println("Good Bye");
     }
