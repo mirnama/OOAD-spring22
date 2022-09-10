24729408680472d1aec932652aa1a2c4ce7659d0
Mirna <70171117+mirnama@users.noreply.github.com>
2022-09-10 16:57:46 -0600

    Analyzer class

--- ptoject1part2/src/Analyzer.java
+++ ptoject1part2/src/Analyzer.java
@@ -6,11 +6,15 @@ public class Analyzer {
     double mean;
     double median;
     double variance, sd,x;
+
     public void analyze(ArrayList<Integer> data){
         for (Integer integer : data) {
             sum += integer;
+
         }
         mean=sum/data.size();
+        int minValu = data.get(0);
+        int maxValu = data.get(0);
         System.out.println("The mean is: "+mean);
         Collections.sort(data);
         int half = data.size()/2;
@@ -20,18 +24,31 @@ public class Analyzer {
         System.out.println("Median:  "+median);
         for (int i =0;  i< data.size();i++){
             x += Math.pow(data.get(i) - mean, 2);
-//            System.out.println("x:"+i+"  "+x);
-        }
-        variance = (x /data.size());
 
+            if (data.get(i)> maxValu){
+                maxValu = data.get(i);
+
+            }else if (data.get(i)< minValu){
+                minValu = data.get(i);
+            }
+
+        }
+
+        variance = (x /data.size());
         sd = Math.sqrt(variance);
         System.out.println("Stander Deviation: "+sd);
         System.out.println("Variance:  "+variance);
+        System.out.println("Maximum Value: "+maxValu);
+        System.out.println("Minimum Value:  "+minValu);
+
 
 
     }
+
+
 }
 
 
 ////https://www.javatpoint.com/how-to-sort-arraylist-in-java
 ////https://stackoverflow.com/questions/4455873/java-arraylist-to-store-user-input
+//https://www.programiz.com/java-programming/examples/standard-deviation
