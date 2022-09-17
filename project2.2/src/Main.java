0e568b2fe77908e8ca42e616cef4555555f95d5f
laurenaparedes <70337912+laurenaparedes@users.noreply.github.com>
2022-09-17 14:14:59 -0600

    Start For Main Game Class

--- project2.2/src/Main.java
+++ project2.2/src/Main.java
@@ -1,5 +1,12 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+import java.util.ArrayList;
+
+
+public class Game {
+    ArrayList<Room> Temple= new ArrayList<Room>();
+    ArrayList<Creature> Creatures= new ArrayList<Creature>();
+    ArrayList<Adventurer> Adventurers= new ArrayList<Adventurer>();
+    public class Turn{}
+    public class Room{}
+    public class Creature{}
+    public class Adventurer{}
 }
