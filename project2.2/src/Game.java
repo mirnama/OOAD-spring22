c45a8e99680f446e12019fd51bff39c745fa83ce
Mirna <70171117+mirnama@users.noreply.github.com>
2022-09-19 10:26:44 -0600

    push my branch

---
+++ project2.2/src/Game.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class Game {
+    ArrayList<Room> Temple= new ArrayList<Room>();
+    ArrayList<Creature> Creatures= new ArrayList<Creature>();
+    ArrayList<Adventurer> Adventurers= new ArrayList<Adventurer>();
+    public class Turn{}
+    public class Room{}
+    public class Creature{}
+    public class Adventurer{}
+}
