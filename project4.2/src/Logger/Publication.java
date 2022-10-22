fe9bab9f1d56bc4e4eac071b505189b6347b1a61
Mirna <70171117+mirnama@users.noreply.github.com>
2022-10-21 20:58:33 -0600

    project4

---
+++ project4.2/src/Logger/Publication.java
@@ -0,0 +1,36 @@
+package Logger;
+
+import Adventurer.Adventurer;
+import Room.Turn;
+
+import java.io.File;
+import java.io.FileWriter;
+
+import static java.awt.SystemColor.info;
+
+public class Publication implements Observer, Logger {
+    Turn t = new Turn();
+    public Publication(Turn t2) {
+        Adventurer a;
+
+    }
+    
+
+    @Override
+    public void update(String s, Turn t) {
+
+        try {
+            FileWriter outPut = new FileWriter("output.txt", true);
+            outPut.write("\n Publisher updates1: "+t+"\n" );
+            outPut.close();;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+//        out("Publisher updates: "+ toString(t));
+    }
+
+    private String toString(Turn t) {
+        return t.toString();
+    }
+}
---
+++ project4.2/src/combat/Combat.java
@@ -0,0 +1,5 @@
+package combat;
+
+public interface Combat {
+    public String fight();
+}
