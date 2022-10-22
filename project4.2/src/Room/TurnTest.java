18bc598a3c5a82fc6644c7a3c55e49045698abd4
Mirna <70171117+mirnama@users.noreply.github.com>
2022-10-21 21:56:22 -0600

    added tests for Careless and Turn

---
+++ project4.2/src/Room/TurnTest.java
@@ -0,0 +1,34 @@
+package Room;
+
+import org.junit.jupiter.api.Test;
+
+import static java.lang.System.out;
+import static org.junit.jupiter.api.Assertions.*;
+
+class TurnTest {
+
+    @Test
+    void getRoomFromTemple() {
+        out.println("get room from temple works");
+    }
+
+    @Test
+    void startGame() {
+    }
+
+    @Test
+    void getTotalTreasure() {
+    }
+
+    @Test
+    void getCreaturesNames() {
+    }
+
+    @Test
+    void getCreatureCount() {
+    }
+
+    @Test
+    void checkFights() {
+    }
+}
