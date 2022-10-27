be84dacdf9116eb74663228544ae7ae38b0e9e23
Mirna <70171117+mirnama@users.noreply.github.com>
2022-10-27 01:02:49 -0600

    adding project 4.2

---
+++ project4/scr/Search/CarefulTest.java
@@ -0,0 +1,18 @@
+package Search;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CarefulTest {
+
+    @org.junit.jupiter.api.Test
+    void getMinDiceRoll() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getAvoidTrap() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void search() {
+    }
+}
---
+++ project4/scr/Search/CarelessTest.java
@@ -0,0 +1,38 @@
+package Search;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+
+import org.junit.jupiter.api.Test;
+
+import static java.lang.System.out;
+import static org.junit.jupiter.api.Assertions.*;
+
+class TurnTest {
+
+    @Test
+    void getRoomFromTemple() {
+        out.println("get room from temple works");
+    }
+
+    @Test
+    void startGame() {
+    }
+
+    @Test
+    void getTotalTreasure() {
+    }
+
+    @Test
+    void getCreaturesNames() {
+    }
+
+    @Test
+    void getCreatureCount() {
+    }
+
+    @Test
+    void checkFights() {
+    }
+}
